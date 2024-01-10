5d02ad62f0cfbfa302e7a266c8d690ea9a9e3cb9
ToniDAWSurface <tonibarcelo@outlook.es>
2024-01-10 13:29:10 +0100

    jugando....

--- activities/UD6A1/jquery.js
+++ activities/UD6A1/jquery.js
@@ -1,8 +1,21 @@
-$("input").on("keyup", function(){
+// VARIABLES
 
+console.log($("#number1").is(':empty'));
+console.log($("#number2").is(':empty'));
+$("input").on("keyup", function(){
     let num1 = $("#number1").val();
     let num2 = $("#number2").val();
+    
+    console.log(!$("#number1").is(':empty'));
+    console.log(num2);
+    
+if($("#number1").is(':empty') && $("#number2").is(':empty')){
 
+console.log('Hola');
+    $('#total').html('88');
+    $('#total').show();
+
+}
 
 
 
