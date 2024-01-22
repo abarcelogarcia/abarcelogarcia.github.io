b5443d1ff524ac3a656b76eada06672bdcec5a2e
Casa <tonibarcelo@outlook.es>
2024-01-22 18:38:49 +0100

    continuando

--- activities/UD6A4/script.js
+++ activities/UD6A4/script.js
@@ -1,50 +1,34 @@
-JQuery.fn.addPostIt = function (id) {
+jQuery.fn.addPostIt = function (id) {
+        postIt = $(this);
+        postIt.data("id", id);
 
-  postIt = $(this);
+        postIt.on("click", function (e) {
+          e.preventDefault();
+          postIt = $(this);
+          id = postIt.data("id");
+          data = '<div class="card text-bg-primary mb-3 ui-widget-content postIt" id="' + id + '">' +
+            '<div class="card-header">Header</div>' +
+            '<div class="card-body">' +
+            '<p class="card-text">New task body</p>' +
+            '</div>' +
+            '</div>';
+          id++;
+          postIt.data("id", id);
+          newPostIt = $(data);
+          $('#postItCreator').prepend(newPostIt.draggable());
 
-  postIt.data("id", id);
-
-
-  postIt.on("click", function (e) {
-
-
-    e.preventDefault();
-
-    postIt = $(this);
-    id = postIt.data("id");
-
-    data = '<div class="card text-bg-primary mb-3 ui-widget-content postIt" id="' + id + '">' +
-      '<div class="card-header">Header</div>' +
-      '<div class="card-body">' +
-      '<p class="card-text">New task body</p>' +
-      '</div>' +
-      '</div>';
-
-    id++;
-    postIt.data("id", id);
-
-    newPostIt = $(data);
-    postIt.append("#postItCreator");
+        })
 
 
+        return this;
+
+        }
 
 
-  })
+        $(function () {
+
+             $('#newPostIt').addPostIt(1);
 
 
-  return this;
-
-}
-
-
-$(function () {
-
-  $(".postIt")
-    .draggable();
-
-  $('#newPostIt').addPostIt(1);
-
-
-});
-
+        });
 
