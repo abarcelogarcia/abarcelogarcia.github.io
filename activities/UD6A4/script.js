1cc2b4d227baabfbb15697725ff759990315d716
ToniDAW <tonibarcelo@outlook.es>
2024-01-23 13:55:57 +0100

    siguiendo UD6A4

--- activities/UD6A4/script.js
+++ activities/UD6A4/script.js
@@ -1,34 +1,55 @@
 jQuery.fn.addPostIt = function (id) {
-        postIt = $(this);
-        postIt.data("id", id);
+  postIt = $(this);
+  postIt.data("id", id);
 
-        postIt.on("click", function (e) {
-          e.preventDefault();
-          postIt = $(this);
-          id = postIt.data("id");
-          data = '<div class="card text-bg-primary mb-3 ui-widget-content postIt" id="' + id + '">' +
-            '<div class="card-header">Header</div>' +
-            '<div class="card-body">' +
-            '<p class="card-text">New task body</p>' +
-            '</div>' +
-            '</div>';
-          id++;
-          postIt.data("id", id);
-          newPostIt = $(data);
-          $('#postItCreator').prepend(newPostIt.draggable());
+  postIt.on("click", function (e) {
+    e.preventDefault();
+    postIt = $(this);
+    id = postIt.data("id");
+    data = '<div class="card text-bg-primary mb-3 ui-widget-content postIt" id="' + id + '">' +
+      '<div class="card-header">' + toDay() + '</div>' +
+      '<div class="card-body">' +
+      '<input type=text class="postItTitle" placeholder="Task Title">' +
+      '<textarea type=text rows="4" cols="40" placeholder="Task details">' +
+      '' +
+      '</textarea>' +
+      '</div>' +
+      '</div>';
+    id++;
+    postIt.data("id", id);
+    newPostIt = $(data);
+    $('#postItCreator').prepend(newPostIt.draggable());
 
-        })
-
-
-        return this;
-
-        }
+  })
 
 
-        $(function () {
+  return this;
 
-             $('#newPostIt').addPostIt(1);
+}
 
 
-        });
+$(function () {
+
+  $('#newPostIt').addPostIt(1);
+
+
+});
+
+
+function toDay() {
+
+  var d = new Date();
+
+  var month = d.getMonth() + 1;
+  var day = d.getDate();
+
+  var output = d.getFullYear() + '/' +
+    (month < 10 ? '0' : '') + month + '/' +
+    (day < 10 ? '0' : '') + day;
+
+  return output;
+
+
+
+}
 
