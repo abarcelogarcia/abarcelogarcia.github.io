b823d58756cd1a514e14d2d9d2c990be622c3075
ToniDAW <tonibarcelo@outlook.es>
2024-02-09 08:59:57 +0100

    UD7A1: implantado tabla de lista de posts. Save funcionando.

--- activities/UD7A1/js/vue.js
+++ activities/UD7A1/js/vue.js
@@ -1,32 +1,81 @@
 const { createApp } = Vue;
 
+let posts2 = [];
 let app = createApp({
-   data() {
-     return {
+  data() {
+    return {
 
-      form:{
+      form: {
 
-        title:'',
-        summary:'',
-        content:'',
-        autor:'',
-        posts: []
+        title: '',
+        summary: '',
+        content: '',
+        autor: ''
 
-        
-      }
-     }
-   },
-   methods:{
-    savePost: function(e){
 
-      let post = form.title;
+      },
+      posts: [
 
-      console.log(post);
-      
+        {
+
+          title: 'My frist Post',
+          summary: 'This post is simply a test',
+          content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit." +
+            "Consectetur laborum unde mollitia libero, a perspiciatis numquam. " +
+            "Itaque enim dolores maiores harum optio similique a tempora officia, autem sit nam maxime.",
+          date: today()
+
+          // autor: this.form.autor
+
+        }
+
+
+      ]
+
+    }
+  },
+  methods: {
+    savePost: function (e) {
+
+      let post = {
+
+        title: this.form.title,
+        summary: this.form.summary,
+        content: this.form.content,
+        date: today()
+
+        // autor: this.form.autor
+
+      };
+
+
+
+
+      this.posts.push(post);
+
+      console.log(this.posts);
+
     }
   }
 
 
 
-   
- }).mount('#app')
+
+}).mount('#app')
+
+
+function today() {
+
+  var d = new Date();
+  var month = d.getMonth() + 1;
+  var day = d.getDate();
+
+  var output =
+
+    (day < 10 ? '0' : '') + day + '/' +
+    (month < 10 ? '0' : '') + month + '/' +
+    d.getFullYear();
+
+  return output;
+
+}
