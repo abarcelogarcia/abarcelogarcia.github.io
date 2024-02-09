7d4e820ec67e21e671fd27b330f4b2fccad37d06
ToniDAWSurface <tonibarcelo@outlook.es>
2024-02-09 17:20:11 +0100

    Uf!

--- activities/UD7A1/js/vue.js
+++ activities/UD7A1/js/vue.js
@@ -1,78 +1,90 @@
 const { createApp } = Vue;
 
-let id = 1
-
-
 let app = createApp({
   data() {
     return {
-
-
       form: {
-        id:'',
-        title: '',
-        summary: '',
-        content: '',
-        content: '',
-        autor: ''
+        id: "",
+        title: "",
+        summary: "",
+        content: "",
+        isEditing: false,
+        autor: "",
       },
 
-      posts: [{
-        id:1,
-        title: 'My frist Post',
-        summary: 'This post is simply a test',
-        content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit." +
-          "Consectetur laborum unde mollitia libero, a perspiciatis numquam. " +
-          "Itaque enim dolores maiores harum optio similique a tempora officia, autem sit nam maxime.",
-        date: today()
-        // autor: this.form.autor
-      }]
-    }
+      posts: [
+        {
+          id: 1,
+          title: "My frist Post",
+          summary: "This post is simply a test",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit." +
+            "Consectetur laborum unde mollitia libero, a perspiciatis numquam. " +
+            "Itaque enim dolores maiores harum optio similique a tempora officia, autem sit nam maxime.",
+          date: today(),
+          isEditing: false
+          // autor: this.form.autor
+        },
+      ],
+    };
   },
   methods: {
     savePost: function (e) {
 
-      let post = {
-        id: (this.posts.slice(-1)[0].id)+1,
+      console.log();
+
+
+        let post = {
+        id: this.posts.slice(-1)[0].id + 1,
         title: this.form.title,
         summary: this.form.summary,
         content: this.form.content,
-        date: today()
+        date: today(),
+        isEditing: false
         // autor: this.form.autor
-
-      }
+      };
 
       this.posts.push(post);
-      console.log(this.posts.slice(-1));
-      // id++;
+      console.log();
     },
-    editPost: function(e){
+    editPost: function (post) {
+      
+      this.form.title = post.title;
+      this.form.summary = post.summary;
+      this.form.content = post.content;
+      this.form.date = today();
+      post.isEditing = true;
 
-      console.log(this.posts.slice(-1)[0].id);
-  
-  
+      console.log(post);
+    },
+
+    updatePost: function (post) {
+
+      console.log(post);
+
+      post.title = this.form.title;
+      post.summary = this.form.summary;
+      post.content = this.form.content;
+      post.date = this.form.date;
+      post.isEditing = false;
+      
     }
-  }
-
-  
-}).mount('#app');
-
-
-
-
+  },
+}).mount("#app");
 
 function today() {
-
   var d = new Date();
   var month = d.getMonth() + 1;
   var day = d.getDate();
 
   var output =
-
-    (day < 10 ? '0' : '') + day + '/' +
-    (month < 10 ? '0' : '') + month + '/' +
+    (day < 10 ? "0" : "") +
+    day +
+    "/" +
+    (month < 10 ? "0" : "") +
+    month +
+    "/" +
     d.getFullYear();
 
   return output;
-
 }
