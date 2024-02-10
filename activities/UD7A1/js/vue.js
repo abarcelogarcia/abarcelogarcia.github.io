7a2f2b40673fce7f24bb8eba94166898c379afad
Casa <tonibarcelo@outlook.es>
2024-02-10 10:38:36 +0100

    Implantado el CRUD

--- activities/UD7A1/js/vue.js
+++ activities/UD7A1/js/vue.js
@@ -8,7 +8,7 @@ let app = createApp({
         title: "",
         summary: "",
         content: "",
-        isEditing: false,
+        image: '',
         autor: "",
       },
 
@@ -22,53 +22,77 @@ let app = createApp({
             "Consectetur laborum unde mollitia libero, a perspiciatis numquam. " +
             "Itaque enim dolores maiores harum optio similique a tempora officia, autem sit nam maxime.",
           date: today(),
-          isEditing: false
+          // isEditing: false
           // autor: this.form.autor
         },
       ],
+      isEditing: false,
+      editingIndex:'',
     };
   },
   methods: {
     savePost: function (e) {
 
-      console.log();
-
-
-        let post = {
+      this.posts.push({
         id: this.posts.slice(-1)[0].id + 1,
         title: this.form.title,
         summary: this.form.summary,
         content: this.form.content,
         date: today(),
-        isEditing: false
+        image: this.form.image
         // autor: this.form.autor
-      };
+      });
 
-      this.posts.push(post);
-      console.log();
+      console.log(this.posts);
     },
-    editPost: function (post) {
+    editPost: function (post, index) {
       
       this.form.title = post.title;
       this.form.summary = post.summary;
       this.form.content = post.content;
       this.form.date = today();
-      post.isEditing = true;
+      this.isEditing = true;
+      this.editingIndex = index;
 
-      console.log(post);
+      console.log(post.id);
     },
 
-    updatePost: function (post) {
+    updatePost: function () {
 
-      console.log(post);
-
-      post.title = this.form.title;
-      post.summary = this.form.summary;
-      post.content = this.form.content;
-      post.date = this.form.date;
-      post.isEditing = false;
+      this.posts[this.editingIndex].title = this.form.title;
+      this.posts[this.editingIndex].summary = this.form.summary;
+      this.posts[this.editingIndex].content = this.form.content;
+      this.posts[this.editingIndex].date = this.form.date;
+      this.isEditing = false;
+      this.editingIndex = '';
       
-    }
+    },
+
+    deletePost: function(index){
+
+      console.log(this.posts[index].title);
+
+
+      this.posts.splice(index, 1);
+
+
+    },
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      var image = new Image();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
   },
 }).mount("#app");
 
